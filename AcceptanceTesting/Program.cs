0b05d80a262271bfe0e101291219786585a65edb
andypaxo <andy@andypaxo.net>
2011-09-01 19:09:30 -0600

    Splitting and outputting lines

--- AcceptanceTesting/Program.cs
+++ AcceptanceTesting/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace AcceptanceTesting
+﻿using System;
+
+namespace AcceptanceTesting
 {
     class Program
     {
+        private const string input =
+@"Given I want to do things
+Then things should be done";
+
         static void Main(string[] args)
         {
+            var testRunner = new TestRunner
+            {
+                OutputStream = Console.OpenStandardOutput()
+            };
+            testRunner.Run(input);
+
+            Console.ReadKey();
         }
     }
 }
